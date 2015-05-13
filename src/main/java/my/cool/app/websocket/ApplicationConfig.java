41ec805677259ccb927ecabdc42add2078af878b
mitsui <mitsui.yasuyuki@simplex-cn.co.jp>
2015-05-13 18:48:03 +0900

    websocketの導入

---
+++ src/main/java/my/cool/app/websocket/ApplicationConfig.java
@@ -0,0 +1,20 @@
+package my.cool.app.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class ApplicationConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry)
+    {
+        registry.addHandler(new EchoHandler(), "/echo").withSockJS();
+        // セッションごとにオブジェクトを分ける場合は以下のように書く
+        // registry.addHandler(new PerConnectionWebSocketHandler(XxxHandler.class), "/echo");
+    }
+
+}
