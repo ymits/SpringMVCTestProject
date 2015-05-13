cce5e8d1036d42fe1b8041dc381bb8dbaa05ac71
mitsui <mitsui.yasuyuki@simplex-cn.co.jp>
2015-05-13 16:40:54 +0900

    テスト実行できるまで

---
+++ src/test/java/my/cool/app/SpringRestDocumentationTest.java
@@ -0,0 +1,47 @@
+package my.cool.app;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ComponentScan("my.cool.app")
+@ContextConfiguration(loader = AnnotationConfigWebContextLoader.class, classes = {WebConfig.class})
+public class SpringRestDocumentationTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private RequestMappingHandlerMapping handlerMapping;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() throws Exception {
+        Assert.assertTrue(false);
+    }
+
+}
