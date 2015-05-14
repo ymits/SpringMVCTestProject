5c4b4c1b1c9c1547cdc7df86bfa4111d1a30f5b5
mitsui <mitsui.yasuyuki@simplex-cn.co.jp>
2015-05-14 18:09:47 +0900

    個人送信

--- src/main/java/my/cool/app/websocket/WSTestController.java
+++ src/main/java/my/cool/app/websocket/WSTestController.java
@@ -1,19 +1,43 @@
 package my.cool.app.websocket;
 
+import java.security.Principal;
+
 import my.cool.app.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WSTestController {
+	
+	@Autowired
+	private SimpMessagingTemplate messagingTemplate;
 
-	@MessageMapping("/test")
-    @SendTo("/topic/testfeed")
-    public Test publish(Test test) throws Exception {
+	@MessageMapping("/no-secure-test")
+	@SendTo("/topic/result")
+    public Test publish1(Test test) throws Exception {
 		System.out.println(test);
-        Thread.sleep(3000); // simulated delay
         return test;
+    }
+	
+	@Secured("ROLE_USER")
+	@MessageMapping("/secure-test")
+	@SendTo("/topic/result")
+    public Test publish2(Test test) throws Exception {
+		System.out.println(test);
+        return test;
+    }
+	
+//	@Secured("ROLE_USER")
+	@MessageMapping("/user-test")
+//    @SendToUser("/queue/user-test")
+    public void executeTrade(Test test) {
+		System.out.println(test);
+//        return test;
+		messagingTemplate.convertAndSendToUser("admin", "/queue/user-test", test);
     }
 }
--- src/main/java/my/cool/app/websocket/WebSocketConfig.java
+++ src/main/java/my/cool/app/websocket/WebSocketConfig.java
@@ -1,11 +1,11 @@
 package my.cool.app.websocket;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -13,13 +13,32 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/topic");
+		config.enableSimpleBroker("/topic", "/queue");
 		config.setApplicationDestinationPrefixes("/api");
 	}
-	
+
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/notification").withSockJS();
+		registry.addEndpoint("/notification")
+//				.addInterceptors(new HttpSessionHandshakeInterceptor())
+				.withSockJS();
 	}
 
 }
+
+//public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfigurer<ExpiringSession> {
+//
+//	@Override
+//	public void configureMessageBroker(MessageBrokerRegistry config) {
+//		config.enableSimpleBroker("/topic", "/queue");
+//		config.setApplicationDestinationPrefixes("/api");
+//	}
+//
+//	@Override
+//	public void configureStompEndpoints(StompEndpointRegistry registry) {
+//		registry.addEndpoint("/notification")
+////				.addInterceptors(new HttpSessionHandshakeInterceptor())
+//				.withSockJS();
+//	}
+//
+//}
