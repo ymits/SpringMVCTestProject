8bf2e640a9469abdfe49a6ed605bf544b2345c11
mitsui <mitsui.yasuyuki@simplex-cn.co.jp>
2015-05-14 20:19:41 +0900

    session切断時にsocketも切断する

--- src/main/java/my/cool/app/session/MySessionRepository.java
+++ src/main/java/my/cool/app/session/MySessionRepository.java
@@ -1,12 +1,22 @@
 package my.cool.app.session;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.session.ExpiringSession;
 import org.springframework.session.MapSessionRepository;
+import org.springframework.session.events.SessionDestroyedEvent;
 import org.springframework.stereotype.Component;
 
 @Component("sessionRepository")
 public class MySessionRepository extends MapSessionRepository{
+	private static final Log logger = LogFactory.getLog(MySessionRepository.class);
 
+	@Autowired
+	private ApplicationEventPublisher eventPublisher;
+	
 	@Override
 	public ExpiringSession createSession() {
 		System.out.println("createSession");
@@ -14,15 +24,25 @@ public class MySessionRepository extends MapSessionRepository{
 	}
 
 	@Override
-	public void delete(String id) {
+	public void delete(String sessionId) {
 		System.out.println("delete");
-		super.delete(id);
+		super.delete(sessionId);
+		publishEvent(new SessionDestroyedEvent(this, sessionId));
+	}
+
+	private void publishEvent(ApplicationEvent event) {
+		try {
+			this.eventPublisher.publishEvent(event);
+		}
+		catch (Throwable ex) {
+			logger.error("Error publishing " + event + ".", ex);
+		}
 	}
 
 	@Override
-	public ExpiringSession getSession(String id) {
+	public ExpiringSession getSession(String sessionId) {
 		System.out.println("getSession");
-		return super.getSession(id);
+		return super.getSession(sessionId);
 	}
 
 	@Override
--- src/main/java/my/cool/app/websocket/WebSocketConfig.java
+++ src/main/java/my/cool/app/websocket/WebSocketConfig.java
@@ -2,14 +2,14 @@ package my.cool.app.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.session.ExpiringSession;
+import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfigurer<ExpiringSession> {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -18,27 +18,10 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 	}
 
 	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
+	public void configureStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/notification")
 //				.addInterceptors(new HttpSessionHandshakeInterceptor())
 				.withSockJS();
 	}
 
 }
-
-//public class WebSocketConfig extends AbstractSessionWebSocketMessageBrokerConfigurer<ExpiringSession> {
-//
-//	@Override
-//	public void configureMessageBroker(MessageBrokerRegistry config) {
-//		config.enableSimpleBroker("/topic", "/queue");
-//		config.setApplicationDestinationPrefixes("/api");
-//	}
-//
-//	@Override
-//	public void configureStompEndpoints(StompEndpointRegistry registry) {
-//		registry.addEndpoint("/notification")
-////				.addInterceptors(new HttpSessionHandshakeInterceptor())
-//				.withSockJS();
-//	}
-//
-//}
